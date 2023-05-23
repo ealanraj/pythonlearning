eaa946b5b6ea7c6f6eedcfdc9519034f033c4e98
ealanraj <134254761+ealanraj@users.noreply.github.com>
2023-05-23 11:01:02 +0530

    Add files via upload
    print related problems leave letter

---
+++ print function.py
@@ -0,0 +1,23 @@
+print("FROM")
+print("\tE.ALAN RAJ,")
+print("\tNO:8,2ND CROSS,")
+print("\tTEACHERS COLONY,")
+print("\tMOOLAKULAM,")
+print("\tPONDICHERRY-605010.")
+print()
+print("TO")
+print("\tTHE PRINCIPAL,")
+print("\tABC SCHOOL,")
+print("\tPONDICHERRY-605005")
+print()
+print("RESPECTED SIR,")
+print("\tAs I was suffering from fever, I'm not able to attend the class. so kindly grant me leave for 2 days.")
+print("\t")
+print("\t\tTHANKING YOU")
+print("\t")
+print("PLACE: PONDICHERRY\tYours faithfully,")
+print("DATE:23/05/2023\t\t   E.ALAN RAJ")
+
+
+
+
