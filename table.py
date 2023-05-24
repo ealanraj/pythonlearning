a4f6c24e0aa8f5bda12d5b287b14050a2a5d8255
ealanraj <134254761+ealanraj@users.noreply.github.com>
2023-05-24 11:12:04 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,7 @@
+print("▬"*42)
+print(" S.NO |\tNAME   | AGE |\tE-MAIL ID\t |")
+print("▬"*42)
+print("  1   |\tAlan   | 20  |\talan@gmail.com\t |")
+print("  2   |\tHarish | 19  |\tharish@gmail.com |")
+print("  3   |\tNithish| 25  |\tnithish@gmail.com|")
+print("▬"*42)
