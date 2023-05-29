09d7c8f112d8da1cefbc7bb9cd41deea65180154
ealanraj <134254761+ealanraj@users.noreply.github.com>
2023-05-29 11:05:30 +0530

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,69 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: D:/python leaning/workings.py =
+8
+
+== RESTART: D:/python leaning/workings.py =
+8
+2
+3
+4
+
+== RESTART: D:/python leaning/workings.py =
+8
+2
+3
+4
+apple
+banana
+cherry
+
+
+== RESTART: D:/python leaning/workings.py =
+8
+2
+3
+4
+apple
+banana
+cherry
+
+== RESTART: D:/python leaning/workings.py =
+<class 'int'>
+
+== RESTART: D:/python leaning/workings.py =
+<class 'int'>
+<class 'float'>
+Traceback (most recent call last):
+  File "D:/python leaning/workings.py", line 6, in <module>
+    print(type(C))
+NameError: name 'C' is not defined. Did you mean: 'c'?
+
+== RESTART: D:/python leaning/workings.py =
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'list'>
+<class 'bool'>
+<class 'tuple'>
+<class 'set'>
+>>> 
+== RESTART: D:/python leaning/workings.py =
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'list'>
+<class 'bool'>
+<class 'tuple'>
+<class 'set'>
+>>> 
+== RESTART: D:/python leaning/workings.py =
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'list'>
+<class 'bool'>
+<class 'tuple'>
+<class 'set'>
+<class 'dict'>
