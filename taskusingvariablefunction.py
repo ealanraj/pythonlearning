9e52d55075349cf29d35c85cd03e5cffe556da8e
ealanraj <134254761+ealanraj@users.noreply.github.com>
2023-05-30 11:29:53 +0530

    Add files via upload

---
+++ taskusingvariablefunction.py
@@ -0,0 +1,15 @@
+name="Alan raj"
+age=20
+address="Pondicherry"
+mobile=9344267602
+email="alanraj@gmail.com"
+print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print("\tSTUDENT INFORMATION")
+print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print("  Name\t\t:",name)
+print("  Age\t\t:",age)
+print("  Address\t:",address)
+print("  Mobile\t:",mobile)
+print("  Email ID\t:",email)
+print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
