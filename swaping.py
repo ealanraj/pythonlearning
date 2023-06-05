aa4d1b8513fc38d5e5721f972d9aba3d9a190c1a
ealanraj <134254761+ealanraj@users.noreply.github.com>
2023-06-05 12:33:26 +0530

    Add files via upload

---
+++ swaping.py
@@ -0,0 +1,35 @@
+##To take inputs from the user:
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+##To create a temporary variable and swap the values:
+temp=a
+a=b
+b=temp
+##print program to run:
+print("the value of 'a' is",a)
+print("the value of 'b' is",b)
+print()
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+##To swap value using add and sub:
+a=a+b
+b=a-b
+a=a-b
+##Program:
+print("the value of 'a' is",a)
+print("the value of 'b' is",b)
+print()
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+##To swap value using multiply and Division:
+a=a*b
+b=int(a/b)
+a=int(a/b)
+##Program:
+print("the value of 'a' is",a)
+print("the value of 'b' is",b)
+
+
+
+
+
